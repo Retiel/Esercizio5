338bdcdbb485d0d167226fbaf2785be8e7a5c86a
Retiel <mandocdoc.rayoliver@gmail.com>
2019-03-12 17:16:47 +0100

    Initialized commit: basic structure, no future purpose, only exercise by using

---
+++ src/main/it/polimi/deib/rayoliver/Convertitore.java
@@ -0,0 +1,21 @@
+package it.polimi.deib.rayoliver;
+
+public class Convertitore
+{
+    double ratio;
+
+    public Convertitore() {
+    }
+
+    public Convertitore(double ratio) {
+        this.ratio = ratio;
+    }
+
+    public double getRatio() {
+        return ratio;
+    }
+
+    public void setRatio(double ratio) {
+        this.ratio = ratio;
+    }
+}
---
+++ src/test/it/polimi/deib/rayoliver/ConvertitoreTest.java
@@ -0,0 +1,26 @@
+package it.polimi.deib.rayoliver;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ConvertitoreTest {
+
+    @Before
+    public  void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getRatio() {
+    }
+
+    @Test
+    public void setRatio() {
+    }
+}
